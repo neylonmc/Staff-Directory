11d8d35f30620e85e0c04870d3f82da524629506
Meaghan Neylon <mneylon@richmond.edu>
2021-01-10 18:27:56 -0500

    Updated Navbar title to add bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./components/layout/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarTop from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <NavbarTop
+          title="Initech Staff Directory"
+          icon="fas fa-broadcast-tower"
+        />
       </div>
     );
   }
