87440a2ced22b38e209bce929b60c60208aeb551
Meaghan Neylon <mneylon@richmond.edu>
2021-01-10 17:24:36 -0500

    Imported Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
