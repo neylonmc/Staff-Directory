ab7f94575cbd4a9739353abb3a85994d3731c05f
Meaghan Neylon <mneylon@richmond.edu>
2021-01-10 21:09:14 -0500

    Deleted navbar and added Jumbotron

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "react-bootstrap/Navbar";
+//import Navbar from "react-bootstrap/Navbar";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
 
 function NavbarTop() {
   return (
-    <Navbar bg="dark" variant="dark" style={{ textAlign: "center" }}>
-      <Navbar.Brand>
-        <h1 className="navbar">Initech Staff Directory</h1>
-      </Navbar.Brand>
-    </Navbar>
+    <Jumbotron fluid>
+      <Container>
+        <h1>Fluid jumbotron</h1>
+        <p>
+          This is a modified jumbotron that occupies the entire horizontal space
+          of its parent.
+        </p>
+      </Container>
+    </Jumbotron>
   );
 }
 export default NavbarTop;
