0cdbdfa475c6137787b37725f1112658071c464d
Meaghan Neylon <mneylon@richmond.edu>
2021-01-10 21:14:15 -0500

    Updated h1 and p tag to match the objective.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -8,11 +8,8 @@ function NavbarTop() {
   return (
     <Jumbotron fluid>
       <Container>
-        <h1>Fluid jumbotron</h1>
-        <p>
-          This is a modified jumbotron that occupies the entire horizontal space
-          of its parent.
-        </p>
+        <h1>IniTech Staff Directory</h1>
+        <p>Staff Directory of Initech. Find all employees below.</p>
       </Container>
     </Jumbotron>
   );
