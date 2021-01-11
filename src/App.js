36fafd3b6db6300ab6091a9a983acf9981c0001c
Meaghan Neylon <mneylon@richmond.edu>
2021-01-10 22:14:06 -0500

    imported new files to the App.js file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Users />
       </div>
     );
   }
