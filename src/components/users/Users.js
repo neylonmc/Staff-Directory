bd5d792a48317365762b2f2aa027a137701c8949
Meaghan Neylon <mneylon@richmond.edu>
2021-01-10 23:19:34 -0500

    Deleted state and added map function to display users.

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,40 +1,11 @@
 import React, { Component } from "react";
 import UserItems from "./UserTable";
 
-export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "id",
-        name: "Mary",
-        large: "https://randomuser.me/api/portraits/women/70.jpg",
-        phone: "5407354680",
-        email: "fdsjk@gmail.com",
-        dob: "",
-      },
-      {
-        id: "id",
-        name: "fds",
-        large: "https://randomuser.me/api/portraits/women/70.jpg",
-        phone: "5407354680",
-        email: "fdsjk@gmail.com",
-        dob: "",
-      },
-      {
-        id: "id",
-        name: "Mary",
-        large: "https://randomuser.me/api/portraits/women/70.jpg",
-        phone: "5407354680",
-        email: "fdsjk@gmail.com",
-        dob: "",
-      },
-    ],
-  };
-
+class Users extends Component {
   render() {
     return (
       <div>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <UserItems key={user.id} user={user} />
         ))}
       </div>
