0c8d258f10c024b3e27b6d3a6d9e0f2326c6c88f
Meaghan Neylon <mneylon@richmond.edu>
2021-01-10 22:23:17 -0500

    Deleted container in jumbotron

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Jumbotron from "react-bootstrap/Jumbotron";
-import Container from "react-bootstrap/Container";
 
 function Navbar() {
   return (
     <Jumbotron fluid>
-      <Container>
-        <h1>IniTech Staff Directory</h1>
-        <p>Staff Directory of Initech. Find all employees below.</p>
-      </Container>
+      <h1>IniTech Staff Directory</h1>
+      <p>Staff Directory of Initech. Find all employees below.</p>
     </Jumbotron>
   );
 }
