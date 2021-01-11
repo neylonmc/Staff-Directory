7a7f05eae818a46f3505bce0afda4d6cdbe9f4fd
Meaghan Neylon <mneylon@richmond.edu>
2021-01-10 21:23:49 -0500

    Changed layout.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavbarTop from "./components/layout/Navbar";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavbarTop
-          title="Initech Staff Directory"
-          icon="fas fa-broadcast-tower"
-        />
+        <Navbar />
       </div>
     );
   }
