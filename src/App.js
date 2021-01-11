d8904e209cdff3d3d53d718e07592a3295a25be5
Meaghan Neylon <mneylon@richmond.edu>
2021-01-10 23:17:19 -0500

    Created an API call to bring in the users.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    fetch("https://randomuser.me/api/?results=10")
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          users: response.results,
+          loading: true,
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
