792af7bdfdc3094e8d2c436e0f361cd6a35cd064
Meaghan Neylon <mneylon@richmond.edu>
2021-01-10 22:21:44 -0500

    Changed to functional component

--- src/components/users/UserTable.js
+++ src/components/users/UserTable.js
@@ -1,26 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
-class UserItems extends Component {
-  render() {
-    const { name, large, phone, email, dob } = this.props.user;
-
-    return (
-      <tr>
-        <td>
-          <img
-            src={large}
-            alt=""
-            className="round-img"
-            style={{ width: "60px " }}
-          />
-        </td>
-        <td>{name}</td>
-        <td>{phone}</td>
-        <td>{email}</td>
-        <td>{dob}</td>
-      </tr>
-    );
-  }
-}
+const UserItems = (props) => {
+  const { name, large, phone, email, dob } = props.user;
+  return (
+    <tr>
+      <td>
+        <img
+          src={large}
+          alt=""
+          className="round-img"
+          style={{ width: "60px " }}
+        />
+      </td>
+      <td>{name}</td>
+      <td>{phone}</td>
+      <td>{email}</td>
+      <td>{dob}</td>
+    </tr>
+  );
+};
 
 export default UserItems;
