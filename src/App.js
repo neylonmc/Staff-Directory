d84b42bfca0cbd45fc3cfe2f9bda771d483a2bf3
Meaghan Neylon <mneylon@richmond.edu>
2021-01-11 21:14:09 -0500

    Removed unnecessary code and deleted User component and replaced it with UserItems.

--- src/App.js
+++ src/App.js
@@ -2,37 +2,17 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
+import UserItems from "./components/users/UserTable";
 import Search from "./components/users/Search";
 
 class App extends Component {
-  state = {
-    users: [],
-    loading: false,
-  };
-
-  async componentDidMount() {
-    this.setState({ loading: true });
-
-    fetch("https://randomuser.me/api/?results=10")
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        this.setState({
-          users: response.results,
-          loading: true,
-        });
-      });
-  }
-
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
           <Search />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <UserItems />
         </div>
       </div>
     );
