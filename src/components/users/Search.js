a8359eb715eed31b48a1e306ae2ea7c941192aee
Meaghan Neylon <mneylon@richmond.edu>
2021-01-11 21:54:42 -0500

    Created onChange method to search bar.

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  onChange = (e) => {
+    this.setState({ text: e.target.value });
+  };
   render() {
     return (
       <form className="form">
-        <input type="text" name="text" placeholder="Search users.." />
+        <input
+          type="text"
+          name="text"
+          placeholder="Search users.."
+          value={this.state.text}
+          onChange={this.onChange}
+        />
         <input type="submit" value="Search" className="btn btn-primary" />
       </form>
     );
