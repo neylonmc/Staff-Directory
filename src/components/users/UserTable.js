80f54704103e19f633aff11983ea22da35609053
Meaghan Neylon <mneylon@richmond.edu>
2021-01-11 21:27:14 -0500

    Changed what data would be outputting to table

--- src/components/users/UserTable.js
+++ src/components/users/UserTable.js
@@ -1,36 +1,11 @@
 import React, { Component } from "react";
 
-// const UserItems = (props) => {
-//   const { large, first, email, phone, gender } = props.user;
-
-//   return (
-//     <tr>
-//       <td>
-//         <img
-//           src={large}
-//           alt=""
-//           className="round-img"
-//           style={{ width: "60px " }}
-//         />
-//       </td>
-//       <td>{first}</td>
-//       <td>{phone}</td>
-//       <td>{email}</td>
-//       <td>{gender}</td>
-//     </tr>
-//   );
-// };
-
-// export default UserItems;
 class UserItems extends Component {
   state = {
     users: [],
-    loading: false,
   };
 
   async componentDidMount() {
-    this.setState({ loading: true });
-
     fetch("https://randomuser.me/api/?results=200")
       .then((results) => {
         return results.json();
@@ -40,10 +15,12 @@ class UserItems extends Component {
           return (
             <tr key={pic.results}>
               <img src={pic.picture.medium} alt="" />
-              <td>{pic.name.first}</td>
+              <td>
+                {pic.name.first} {pic.name.last}
+              </td>
               <td>{pic.phone}</td>
               <td>{pic.email}</td>
-              <td>{pic.gender}</td>
+              <td>{pic.dob.age}</td>
             </tr>
           );
         });
