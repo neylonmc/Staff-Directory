4bca6bf6e02e3d010231ea0e0d80b21a27b24959
Meaghan Neylon <mneylon@richmond.edu>
2021-01-13 14:58:17 -0500

    Updated comments

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  //Creates state object
   state = {
     text: "",
   };
 
+  //Allows text to be input in the field
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
+  //When the submit button is hit, changes the state
   onSubmit = (e) => {
     e.preventDefault();
     this.props.searchUsers(this.state.text);
     this.setState({ text: "" });
   };
 
+  //Renders data below
   render() {
     return (
       <form onSubmit={this.onSubmit} className="form">
